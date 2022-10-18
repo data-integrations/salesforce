f73db66d533b089d0b766049fb437932e349d1aa
Shubhangi-cs <shubhangi.garg@cloudsufi.com>
2022-10-18 12:22:13 +0530

    Rollback change

--- src/main/java/io/cdap/plugin/salesforce/plugin/source/batch/SalesforceBaseSourceConfig.java
+++ src/main/java/io/cdap/plugin/salesforce/plugin/source/batch/SalesforceBaseSourceConfig.java
@@ -96,7 +96,7 @@ public abstract class SalesforceBaseSourceConfig extends ReferencePluginConfig {
   @Macro
   @Nullable
   @Description("The existing connection to use.")
-  SalesforceConnectorConfig connection;
+  private SalesforceConnectorConfig connection;
 
   protected SalesforceBaseSourceConfig(String referenceName,
                                        @Nullable String consumerKey,
