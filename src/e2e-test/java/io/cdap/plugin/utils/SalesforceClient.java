335f1356833ab81ae04213039544120077960c75
Vipinofficial11 <vipin.bhatt@cloudsufi.com>
2023-06-30 12:47:03 +0530

    Update the license date.

--- src/e2e-test/java/io/cdap/plugin/utils/SalesforceClient.java
+++ src/e2e-test/java/io/cdap/plugin/utils/SalesforceClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Cask Data, Inc.
+ * Copyright © 2023 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -169,11 +169,9 @@ public class SalesforceClient {
   public static void deletePushTopic(String pushTopicName) {
     try {
       PartnerConnection partnerConnection = new PartnerConnection(
-        Authenticator.createConnectorConfig(new AuthenticatorCredentials(
-          System.getenv("SALESFORCE_USERNAME"), System.getenv("SALESFORCE_PASSWORD") +
-          System.getenv("SALESFORCE_SECURITY_TOKEN"),
-          System.getenv("SALESFORCE_CONSUMER_KEY"), System.getenv("SALESFORCE_CONSUMER_SECRET"),
-          PluginPropertyUtils.pluginProp("login.url"), 30000, "")));
+        Authenticator.createConnectorConfig(new AuthenticatorCredentials(USERNAME, PASSWORD + SECURITYTOKEN,
+          CLIENTID, CLIENTSECRET, PluginPropertyUtils.pluginProp("login.url"),
+          30000, "")));
 
       QueryResult queryResult = SalesforceStreamingSourceConfig.runQuery(partnerConnection,
         String.format("SELECT Id FROM PushTopic WHERE Name = '%s'", pushTopicName));
